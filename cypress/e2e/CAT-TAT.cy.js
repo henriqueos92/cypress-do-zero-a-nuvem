b60620bfd4f774f8794f43e6b8070bf9e8d78a86
Henrique Oliveira <henriqueoliveira@MacBook-Air-de-Henrique.local>
2025-01-22 18:11:49 -0300

    Marcando inputs do tipo radio

--- cypress/e2e/CAT-TAT.cy.js
+++ cypress/e2e/CAT-TAT.cy.js
@@ -137,9 +137,28 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       .should('have.value', 'mentoria')
   })
 
-  it.only('seleciona um produto (Blog) por seu índice', () => {
+  it('seleciona um produto (Blog) por seu índice', () => {
     cy.get('#product')
       .select(1)
       .should('have.value', 'blog')
+  })
+
+  it('marca o tipo de atendimento "Feedback"', () => {
+    cy.get('input[type="radio"][value="feedback"]')
+      .check()
+      .should('be.checked')
+  })
+
+  it.only('marca cada tipo de atendimento', () => {
+    /*cy.get('input[type="radio"][value="ajuda"]').check().should('be.checked')
+    cy.get('input[type="radio"][value="elogio"]').check().should('be.checked')
+    cy.get('input[type="radio"][value="feedback"]').check().should('be.checked')*/
+
+    cy.get('input[type="radio"]')
+      .each(typeOfService => {
+        cy.wrap(typeOfService)
+          .check()
+          .should('be.checked')
+      })
   })
 })
